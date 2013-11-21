b348dd219b70293bae39609464187bbccb7328d3
mavanessadcruz <m.delacruz@hotmail.com>
2013-11-21 15:23:33 +0000

    Hours working

--- owlClock.js
+++ owlClock.js
@@ -6,9 +6,8 @@ function updateClock()
 	var currentSeconds = currentTime.getSeconds();
 	
 	var handRotation = 0; //the degrees
-
 	
-	handRotation = currentSeconds*6; // adds 6 degrees to the rotation
+	handRotation = currentMinutes*6; // adds 6 degrees to the rotation
 	
 	
 	var RightEye = document.getElementById("RightEye"); 
@@ -25,9 +24,34 @@ var RightPupil = document.getElementById("RightPupil");
 	
 	
 	var transformTxt =  "rotate("+ handRotation + ","+cx+","+cy+")"; 
-	console.log (transformTxt); 
+	//console.log (transformTxt); 
 	
 	RightEye.setAttribute('transform',transformTxt);
+
+	
+	
+	
+	handRotation = 360*(currentHours % 12)/12; // adds 6 degrees to the rotation
+	
+	
+	var LeftEye = document.getElementById("LeftEye"); 
+var LeftPupil = document.getElementById("LeftPupil");
+	
+	//var bb = rightPupil.getBoundingBox();
+	
+	//var cx = bb.x + bb.width/2;
+	//var cy = bb.y + bb.height/2;
+	
+	var cxLeft = LeftPupil.getAttribute("cx");
+	var cyLeft = LeftPupil.getAttribute("cy");
+
+	
+	
+	var transformTxtHours =  "rotate("+ handRotation + ","+cxLeft+","+cyLeft+")"; 
+	console.log (transformTxtHours); 
+	
+	LeftEye.setAttribute('transform',transformTxtHours);
+
 	 
 	
 	
